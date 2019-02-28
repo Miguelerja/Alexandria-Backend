ebf9777db40b9639c338146b0ad0218359e6b8d8
Didac Bigorda <didacbigorda@gmail.com>
2019-02-28 12:48:06 +0100

    DB name fix

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 
 const auth = require('./routes/auth');
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.DB_URL, {
   keepAlive: true,
   useNewUrlParser: true,
   reconnectTries: Number.MAX_VALUE,
